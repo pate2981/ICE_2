117ee3019138388ab24e67bb44507980b2e32b06
Sanket <Sanket@192.168.2.54>
2021-02-18 18:35:17 -0500

    First Commit

---
+++ src/cardtrick/CardTrick.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cardtrick;
+
+/**
+ *
+ * @author Sanket
+ */
+public class CardTrick {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int handsize = 52;
+        Card [] cards = new Card [handsize];
+        
+        int countCards = 0;
+	for(Card.Suit s: Card.Suit.values())
+        {
+            for(Card.Value v: Card.Value.values())
+            {
+                cards[countCards] = (new Card(s,v));
+                countCards++;
+            }
+        }
+        
+
+        for(Card c: cards)
+        {
+            System.out.println(c.getValue() + " of " + c.getSuit());
+        }
+
+        
+        
+    }
+    
+}
